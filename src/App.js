e99412a024e86706b09bd05c86fb2d66a97aa892
Martin Gonzalez <martin.hgonzalez21@gmail.com>
2020-02-19 00:42:47 -0300

    Finished API calls (still need to relocate out of App component)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const apiKey = "f48778fa26cf72e5fea94697c9ee8692";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  function getGenresFromAPI() {
+    fetch(
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`
+    )
+      .then(function(res) {
+        return res.json();
+      })
+      .then(function(genres) {
+        console.log(genres);
+      });
+  }
+
+  function getLatestMovie() {
+    fetch(
+      `https://api.themoviedb.org/3/movie/latest?api_key=${apiKey}&language=en-US
+      `
+    )
+      .then(function(res) {
+        return res.json();
+      })
+      .then(function(latestMovie) {
+        console.log(latestMovie);
+      });
+  }
+
+  function getNowPlayingMovies() {
+    fetch(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1    
+      ` //Only page 1.
+    )
+      .then(function(res) {
+        return res.json();
+      })
+      .then(function(nowPlayingMovies) {
+        console.log(nowPlayingMovies.results);
+      });
+  }
+  function getPopularMovies() {
+    fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1    
+        ` //Only page 1.
+    )
+      .then(function(res) {
+        return res.json();
+      })
+      .then(function(popularMovies) {
+        console.log(popularMovies.results);
+      });
+  }
+  function getTopRatedMovies() {
+    fetch(
+      `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1
+    
+        ` //Only page 1.
+    )
+      .then(function(res) {
+        return res.json();
+      })
+      .then(function(topRatedMovies) {
+        console.log(topRatedMovies.results);
+      });
+  }
+
+  function getUpcomingMovies() {
+    fetch(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=en-US&page=1` //Only page 1.
+    )
+      .then(function(res) {
+        return res.json();
+      })
+      .then(function(upComingMovies) {
+        console.log(upComingMovies.results);
+      });
+  }
+
+  function getMoviesBySearch(param) {
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${param}&page=1&include_adult=false` //Only page 1.
+    )
+      .then(function(res) {
+        return res.json();
+      })
+      .then(function(moviesSearched) {
+        console.log(moviesSearched.results);
+      });
+  }
+
+  getGenresFromAPI();
+  getLatestMovie();
+  getNowPlayingMovies();
+  getPopularMovies();
+  getTopRatedMovies();
+  getUpcomingMovies();
+  getMoviesBySearch("pokemon");
+
+  return <div>asd</div>;
 }
 
 export default App;
