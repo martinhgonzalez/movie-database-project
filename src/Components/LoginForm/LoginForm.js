a408763c608664b1bedcd66d8d4551ae11cd51da
Martin Gonzalez <martin.hgonzalez21@gmail.com>
2020-02-26 09:27:16 -0300

    fixes of img not working and background color while autocomplete

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -1,11 +1,10 @@
 import React from "react";
-import './loginForm.css';
+import "./loginForm.css";
 
 class LoginForm extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
   }
-
 
   render() {
     return (
@@ -14,13 +13,24 @@ class LoginForm extends React.Component {
 
         <div className="row">
           <form className="columna" onSubmit={this.props.onSubmit}>
-            <img src="https://icon-icons.com/icons2/933/PNG/128/round-account-button-with-user-inside_icon-icons.com_72596.png"></img>
+            <img
+              className="userImg"
+              src="https://cdn2.iconfinder.com/data/icons/user-icon-2-1/100/user_5-15-512.png"
+              alt="img"
+            ></img>
             {/* E-mail */}
             <div className="row">
               <div className="input-field col s12   ">
                 <i className="material-icons prefix ">email</i>
-                
-                <input type="email" name ="mail" className="validate" value={this.props.formLogin.mailUser} onChange={this.props.onChange} id="email"    />
+
+                <input
+                  type="email"
+                  name="mail"
+                  className="validate"
+                  value={this.props.formLogin.mailUser}
+                  onChange={this.props.onChange}
+                  id="email"
+                />
                 {/* <input type="text" name ="taskName" className="formLogin-control" value={this.props.formLogin.taskName} onChange={this.props.onChange}></input> */}
 
                 <label htmlFor="email">E-mail</label>
@@ -30,7 +40,14 @@ class LoginForm extends React.Component {
             <div className="row">
               <div className="input-field col s6 ">
                 <i className="material-icons prefix ">account_circle</i>
-                <input id="icon_prefix" type="text" name="name" value={this.props.formLogin.nameUser} onChange={this.props.onChange} className="validate " />
+                <input
+                  id="icon_prefix"
+                  type="text"
+                  name="name"
+                  value={this.props.formLogin.nameUser}
+                  onChange={this.props.onChange}
+                  className="validate "
+                />
                 {/* <input type="text" name ="taskDescription" className="formLogin-control" value={this.props.formLogin.taskDescription} onChange={this.props.onChange}></input> */}
 
                 <label htmlFor="icon_prefix ">Alias</label>
@@ -40,14 +57,27 @@ class LoginForm extends React.Component {
             <div className="row">
               <div className="input-field col s6">
                 <i className="material-icons prefix">lock_outline</i>
-                <input id="create" type="password" name="pass"  value={this.props.formLogin.passUser} onChange={this.props.onChange} className="validate" />
+                <input
+                  id="create"
+                  type="password"
+                  name="pass"
+                  value={this.props.formLogin.passUser}
+                  onChange={this.props.onChange}
+                  className="validate"
+                />
                 <label htmlFor="create">Password</label>
               </div>
             </div>
-            <button className="btn waves-effect " type="submit" value='Submit' name="action">GO!</button>
+            <button
+              className="btn waves-effect "
+              type="submit"
+              value="Submit"
+              name="action"
+            >
+              GO!
+            </button>
           </form>
         </div>
-
       </div>
     );
   }
