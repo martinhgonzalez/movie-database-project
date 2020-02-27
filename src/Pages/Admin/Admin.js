2d976a5467c61340dadd727993b33880fc252fc8
Martin Gonzalez <martin.hgonzalez21@gmail.com>
2020-02-26 21:06:40 -0300

    logout buttons added

--- src/Pages/Admin/Admin.js
+++ src/Pages/Admin/Admin.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./admin.css";
 import AddMovieContainer from "../../Components/AddMovieContainer/AddMovieContainer";
 import { getGenresFromAPI } from "../../Services/API";
+import { Redirect } from "react-router-dom";
 
 class Admin extends React.Component {
   constructor() {
     super();
     this.state = {
-      displaying: "none"
+      displaying: "none",
+      loggingOut: false
     };
   }
 
@@ -28,17 +30,35 @@ class Admin extends React.Component {
   };
 
   render() {
+    if (this.state.loggingOut === true) {
+      return <Redirect to={"/login"} />;
+    }
+
     return (
       <>
         <h1 class="center-align">WELCOME ADMIN</h1>
-          <span onClick={() => this.onClickBtn("api")}
-            className="waves-effect waves-light btn-large #212121 grey darken-4" >
-            ADD MOVIES FROM API
+        <span class="logout">
+          <a
+            className=" grey darken-3 btn-small "
+            onClick={() => {
+              this.setState({ loggingOut: true });
+            }}
+          >
+            <i class="material-icons left">vpn_key</i>
+            Logout
+          </a>
+        </span>
+        <span
+          onClick={() => this.onClickBtn("api")}
+          className="waves-effect waves-light btn-large #212121 grey darken-4"
+        >
+          ADD MOVIES FROM API
         </span>
 
         <span
           onClick={() => this.onClickBtn("custom")}
-          value="custom" className="waves-effect waves-light btn-large #212121 grey darken-4"
+          value="custom"
+          className="waves-effect waves-light btn-large #212121 grey darken-4"
         >
           ADD CUSTOM MOVIE
         </span>
