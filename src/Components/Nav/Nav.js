29b66b463983c61879215d477dba55e816a44893
Jazmin <briascojazmin@gmail.com>
2020-02-27 21:10:47 -0300

    seatch-button-hover

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -40,6 +40,7 @@ class Nav extends React.Component {
 
   submitedSearch = e => {
     this.props.onHandleSubmit(e);
+
     this.setState({ selection: 0 });
     e.target.reset();
   };
@@ -90,11 +91,11 @@ class Nav extends React.Component {
                   />
                 </form>
                 </li>
-                <div className="  divNavOptions">
+                <div className="divNavOptions">
                   <li>
                     <div className="searchNav">
-                      <button type="submit" className="  buttonNav #ffffff white btn btn-primary">
-                        Submit
+                      <button type="submit" className="buttonNav waves-effect waves-brown btn">
+                        SUBMIT
                       </button>
                     </div>
                   </li>
